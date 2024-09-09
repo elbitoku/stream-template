84c39b5c53648d8fc1d9e163b05ed2d3030a883f
elbitoku <leo.langlois123@gmail.com>
2024-09-09 19:57:42 +0200

    search

---
+++ script.js
@@ -0,0 +1,7 @@
+let helloNotif = document.querySelector('#helloNotif');
+
+helloNotif.addEventListener("click", () => {
+    console.log("clique");
+    
+})
+// AJOUTER UN POP UP AVEC LES DERNIERES NEWS
