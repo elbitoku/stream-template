6411e88a26ac78c152a4d3d4a20da448b8375f5c
elbitoku <leo.langlois123@gmail.com>
2024-09-09 20:33:36 +0200

    searchbar

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let helloNotif = document.querySelector('#helloNotif');
+let filter = document.querySelector('#filter')
 
 helloNotif.addEventListener("click", () => {
     console.log("clique");
     
 })
 // AJOUTER UN POP UP AVEC LES DERNIERES NEWS
+
+
+filter.addEventListener("click", () => {
+    console.log("clique");
+    
+})
